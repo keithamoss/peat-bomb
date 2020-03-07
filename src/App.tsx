74a58baf670844281bc08a94fa1813c4fe6dbcb5
Keith Moss <keithamoss@gmail.com>
2020-03-07 19:17:24 +0800

    Autocomplete example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-import { Navbar } from './components/Navbar'
+import React, { Fragment } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <Switch>
-        <div className="container">
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-        </div>
+        <Fragment>
+          <div className="container">
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+          </div>
+        </Fragment>
       </Switch>
     </BrowserRouter>
   )
